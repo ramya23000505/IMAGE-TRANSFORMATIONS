ca98acfa4b5d758eb79a9730dfbd7c59db67e5a1
ramya23000505 <149370791+ramya23000505@users.noreply.github.com>
2024-09-30 15:42:35 +0530

    Update IMAGE TRANSFORMATION[4-6].py

--- IMAGE TRANSFORMATION[4-6].py
+++ IMAGE TRANSFORMATION[4-6].py
@@ -8,11 +8,15 @@ input_image=cv2.cvtColor(input_image,)
 plt.axis("off") 
 plt.imshow(input_image)
 plt.show()
-rows, cols, dim = 
-M_x=np.float
-reflected_img_xaxis=cv2.warpPerspective(input_image,M_x,(cols,rows))
-reflected_img_yaxis=cv2.warpPerspective(input_image,M_y,(cols,rows))
-plt.imshow(reflected_)
+rows, cols, dim = 0
+M_x = np.float32([[-1, 0, cols],
+                  [0, 1, 0],
+                  [0, 0, 1]])
+
+reflected_img_xaxis = cv2.warpAffine(input_img, M_x[:2], (cols, rows))
+
+plt.axis("off")
+plt.imshow(reflected_img_xaxis)
 plt.show()
 
 ##v)Image Rotation:
@@ -21,17 +25,24 @@ import cv2
 import matplotlib.pyplot as plt
 input_image=cv2.imread("got.jpg") 
 input_image=cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
-angle=np.radians(45)
-M=np.float32([[np.cos(angle),-(np.sin(angle)),0],
-               [np.
-rotated_img=
+angle = np.radians(-60)
+M = np.float32([[np.cos(angle), -np.sin(angle), 0],
+                [np.sin(angle), np.cos(angle), 0]])
+center = (cols // 2, rows // 2)
+M = cv2.getRotationMatrix2D(center, -60, 1.0)
+rotated_img = cv2.warpAffine(input_img, M, (cols, rows))
+
+plt.axis('off')
+plt.imshow(rotated_img)
+plt.show()
 
 ##vi)Image Cropping:
 
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-input_image=cv2.imread("got.jpg") 
-input_image=cv2.cvtColor(input_image,
+cropped_img = input_img[50:200, 200:400]
+plt.axis('off')
+plt.imshow(cropped_img)
 plt.show()
 
